6622f2591f8ed5d82a0a36f46f4d819e17aae798
AYUSH RAJ <rajayush1230@gmail.com>
2025-06-20 10:04:03 +0530

    tofixed(2) added

--- script.js
+++ script.js
@@ -22,9 +22,10 @@ let covertbtn = document.getElementById("btn-el")
 let lengthEl = document.getElementById("length-el")
 let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
+
 covertbtn.addEventListener("click",function(){
     let basevalue = inputEl.value
-    lengthEl.textContent = `${basevalue} Meters =  ${basevalue*meterToFeet} Feets | ${basevalue} Feets = ${basevalue*0.3048} Meters`
+    lengthEl.textContent = `${basevalue} Meters =  ${(basevalue*meterToFeet).toFixed(2)} Feets | ${basevalue} Feets = ${(basevalue*0.3048).toFixed(2)} Meters`
     volumeEl.textContent = `${basevalue} Liters =  ${basevalue*literToGallon} Gallons | ${basevalue} Gallons = ${basevalue*3.78541} Liters`
     massEl.textContent = `${basevalue} Kilograms =  ${basevalue*kiloToPound} Pounds | ${basevalue} Pounds = ${basevalue*0.453592} Kilograms`
 
